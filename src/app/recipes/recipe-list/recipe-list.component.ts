be6b71beb72fad847081b277f8b0b55597d26f32
BenoitGaudieri <benalek@gmail.com>
2021-01-10 17:10:23 +0100

    🔧 models and basic component structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+// Models
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        // call the constructor
+        new Recipe("Test recipe", "this is a test", "https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg"),
+        new Recipe("Test recipe", "this is a test", "https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg"),
+        
+    ]
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+
+    // automatically assign propriety to the constructor
+    // it's a shortcut
+    constructor(public name: string, public amount: string) {
+
+    }
+}
