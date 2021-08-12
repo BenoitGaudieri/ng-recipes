5c05b61e4cbcb1e14c09515645d591f9598adf7f
BenoitGaudieri <benalek@gmail.com>
2021-08-12 16:21:38 +0200

    recipe switch

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 // Models
 import { Recipe } from '../recipe.model';
@@ -6,19 +6,30 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [
-        // call the constructor
-        new Recipe("Test recipe", "this is a test", "https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg"),
-        new Recipe("Test recipe", "this is a test", "https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg"),
-        
-    ]
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  recipes: Recipe[] = [
+    // call the constructor
+    new Recipe(
+      'Test recipe',
+      'this is a test',
+      'https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg'
+    ),
+    new Recipe(
+      'Test 2',
+      'this is another test',
+      'https://blog.giallozafferano.it/inventaricette/wp-content/uploads/2020/04/panino-con-hamburger-fb.jpg'
+    ),
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
