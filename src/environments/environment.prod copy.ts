91a6018b1df468ae74a27868789cf9a358686688
BenoitGaudieri <benalek@gmail.com>
2021-09-16 17:07:44 +0200

    env and test service

---
+++ src/environments/environment.prod copy.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  //   firebase project api to compile
+  BASE_API: 'https://???.firebasedatabase.app/',
+  API_RECIPE: 'https://???.firebasedatabase.app/recipes.json',
+  API_KEY: '',
+  // Firebase predefined api
+  SIGNUP_API: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  LOGIN_API:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+};
