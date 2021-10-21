6567b0c389a4c294c4cc05f1989de84b3bfcae1d
BenoitGaudieri <benalek@gmail.com>
2021-10-21 16:25:27 +0200

    angular animations

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
@@ -6,22 +6,173 @@ import { Ingredient } from '../shared/ingredient.model';
 // import { ShoppingListService } from './shopping-list.service';
 import * as ShoppingListActions from './store/shopping-list.actions';
 import * as fromApp from '../store/app.reducer';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+  group,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss'],
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          backgroundColor: 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px)',
+        })
+      ),
+      //   transition('normal => highlighted', animate(300)),
+      //   transition('highlighted => normal', animate(100)),
+      transition('highlighted <=> normal', animate(100)),
+    ]),
+
+    trigger('wildState', [
+      state(
+        'normal',
+        style({
+          backgroundColor: 'red',
+          transform: 'translateX(0) scale(1)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          backgroundColor: 'blue',
+          transform: 'translateX(100px) scale(1)',
+        })
+      ),
+      state(
+        'shrunken',
+        style({
+          backgroundColor: 'green',
+          transform: 'translateX(0px) scale(0.5)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange',
+          borderRadius: '0px',
+        }),
+        animate(
+          1000,
+          style({
+            borderRadius: '50px',
+          })
+        ),
+        animate(500),
+      ]),
+    ]),
+
+    trigger('list1', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)',
+        }),
+        animate(300),
+      ]),
+      transition('* => void', [
+        animate(300),
+        style({
+          transform: 'translateX(100px)',
+        }),
+      ]),
+    ]),
+
+    trigger('list2', [
+      state(
+        'in',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)',
+        })
+      ),
+      transition('void => *', [
+        animate(
+          1000,
+          keyframes([
+            style({
+              transform: 'translateX(-100px)',
+              opacity: 0,
+              offset: 0,
+            }),
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0.5,
+              offset: 0.3,
+            }),
+            style({
+              transform: 'translateX(-20px)',
+              opacity: 1,
+              offset: 0.8,
+            }),
+            style({
+              transform: 'translateX(0px)',
+              opacity: 1,
+              offset: 1, // this is the last keyframe
+            }),
+          ])
+        ),
+      ]),
+      transition('* => void', [
+        group([
+          animate(
+            300,
+            style({
+              color: 'red',
+            })
+          ),
+          animate(
+            800,
+            style({
+              transform: 'translateX(100px)',
+              opacity: 0,
+            })
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   //   ingredients: Ingredient[];
   // NGRX
   ingredients: Observable<{ ingredients: Ingredient[] }>;
   private igChangeSub: Subscription;
+  // Angular animations
+  state = 'normal';
+  wildState = 'normal';
+  visibility = 'none';
 
   constructor(
     // private slService: ShoppingListService,
     private loggingService: LoggingService,
-    private store: Store<fromApp.AppState>
+    private store: Store<fromApp.AppState>,
+    private el: ElementRef
   ) {}
 
   ngOnInit() {
@@ -46,5 +197,35 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   onEditItem(index: number) {
     // this.slService.startedEditing.next(index);
     this.store.dispatch(ShoppingListActions.startEdit({ index: index }));
+  }
+
+  onAnimate() {
+    this.state === 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+    this.wildState === 'normal'
+      ? (this.wildState = 'highlighted')
+      : (this.wildState = 'normal');
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onShowCard() {
+    const div = this.el.nativeElement.getElementsByClassName('card')[0];
+    if (div.classList.contains('hidden')) {
+      div.classList.remove('hidden');
+    } else {
+      div.classList.add('hidden');
+    }
+  }
+
+  animationStarted(event: Event) {
+    console.log(event);
+  }
+
+  animationEnded(event: Event) {
+    console.log(event);
   }
 }
